d18764043eeb2227bfbafb634a480b08ec2689a8
Heorhii_Potapov <potaph01@kits.kfplc.com>
2021-06-09 17:43:15 +0300

    [Task 1] - review dependency.

--- algolia/src/main/java/com/epam/algoliaresearch/algolia/Application.java
+++ algolia/src/main/java/com/epam/algoliaresearch/algolia/Application.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import java.security.Security;
+
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
+        Security.setProperty("networkaddress.cache.ttl", "60");
         SpringApplication.run(Application.class);
     }
 }
